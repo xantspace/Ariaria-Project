8660966084d0d2322d6777c9c4243c118b7046ee
Pleasant <johnsonpleasant02@gmail.com>
2025-12-18 07:14:33 +0100

    second commit

--- project_ariaria/settings.py
+++ project_ariaria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sklen!wn)e-)88s5bg_kzq%@2puvcb!2@i7q%5cnpix)d9meu3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "192.168.43.99", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
