2b35dc2777443d9fb1c2fde3d8f4701662b39ad5
Pleasant <johnsonpleasant02@gmail.com>
2025-12-18 16:02:24 +0100

    new commit

--- project_ariaria/settings.py
+++ project_ariaria/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sklen!wn)e-)88s5bg_kzq%@2puvcb!2@i7q%5cnpix)d9meu3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
